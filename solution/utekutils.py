681ac298ef32afaefe66b7378a5c52fbfbfc4126
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-01-13 14:02:46 -0500

    Update lambda values based on new competition package

--- solution/utekutils.py
+++ solution/utekutils.py
@@ -6,7 +6,7 @@ asciiForA = 0x41
 """Constant for the ASCII code of uppercase Z."""
 asciiForZ = 0x5a
 """The hardcoded lambda values for part 3."""
-ptb_prob_weights = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.888889]
+ptb_prob_weights = [1e-5,1e-4,1e-3,1e-2,1e-1,0.88889]
 
 
 def doPart(infilename, partfn):
