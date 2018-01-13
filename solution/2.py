b3b7e1dc88321e7e47de79220c570abe0e3a67c4
Johnson Zhong <johnsonzhong@hotmail.ca>
2018-01-13 13:43:18 -0500

    Model ngrams and get sentence scores (2b)

--- solution/2.py
+++ solution/2.py
@@ -1,5 +1,10 @@
 import re
 import pickle
+import math
+
+
+def sanitize_input(text):
+    return re.sub(r'<unk>|[^\w]', '', text).upper()
 
 
 def sanitize_without_space(text):
@@ -29,8 +34,9 @@ def count_chars(corpus, maxn):
 MAX_N = 7
 PTB_SRC = "../ptb.train.txt"
 
-
 ptb_ngrams = None
+
+
 def get_ptb_ngrams():
     global ptb_ngrams
     if ptb_ngrams:
@@ -49,7 +55,7 @@ def get_ptb_ngrams():
     return ngrams
 
 
-def get_prob(ngrams, word):
+def get_count(ngrams, word):
     ngram = ngrams[len(word) - 1]
     if word not in ngram:
         return 0
@@ -57,10 +63,42 @@ def get_prob(ngrams, word):
         return ngram[word]
 
 
+def get_ptb_sentence_score(sentence, weights):
+    # assumes sentence is sanitized
+    # split sentence into MAX_N grams and take the sum of the log prob
+    prob = 0
+    for i in range(len(sentence) - MAX_N + 1):
+        word = sentence[i:i + MAX_N]
+        print("{}: {}".format(i, word))
+        p = get_ptb_prob(word, weights)
+        print("logp: {}".format(p))
+        if p != 0:
+            prob += math.log(p)
+
+    return prob
+
+
+def get_ptb_prob(word, weights):
+    ngrams = get_ptb_ngrams()
+    prob = 0
+    for i in range(1, len(word)):
+        denom = get_count(ngrams, word[:i])
+        num = get_count(ngrams, word[:i + 1])
+        print("{}:{}\t{}:{}".format(word[:i + 1], num, word[:i], denom))
+
+        # anything else will also be 0
+        if denom == 0:
+            break
+
+        prob += weights[i] * num / denom
+
+    return prob
+
+
 def part_2a(line, output):
     line = [side.strip() for side in line.split("|")]
     corpus = line[0]
-    word = re.sub(r'<unk>|[^\w]', '', line[1])
+    word = sanitize_input(line[1])
 
     print("[{}] [{}]".format(corpus, word))
 
@@ -69,20 +107,42 @@ def part_2a(line, output):
     else:
         ngrams = count_chars(corpus, MAX_N)
 
-    print(get_prob(ngrams, word))
-    print(get_prob(ngrams, word), file=output)
+    print(get_count(ngrams, word))
+    print(get_count(ngrams, word), file=output)
 
 
-def get_ptb_prob(string):
-    return 0
+def part_2b(line, output):
+    line = [sanitize_input(sent.strip()) for sent in line.split("|")]
+    most_likely_prob = -math.inf
+    most_likely_sentence = None
+
+    WEIGHTS = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.888889]
+
+    for i in range(len(line)):
+        sentence = line[i]
+        logp = get_ptb_sentence_score(sentence, WEIGHTS)
+        print("---\n{}:{}\n".format(sentence, logp))
+        if logp > most_likely_prob:
+            most_likely_prob = logp
+            most_likely_sentence = i + 1
+
+    print(most_likely_sentence, file=output)
 
 
-# with open(PTB_SRC, 'r') as src:
-#     # print(sanitize_without_space(src.read()))
-#     ngrams = get_ptb_ngrams()
-if __name__ == "__main__":
+def do_part_2a():
     with open("../input/2a.in", 'r') as input:
         with open("../output/2a.out", 'w') as output:
             for line in input:
-                print(line)
                 part_2a(line, output)
+
+
+def do_part_2b():
+    with open("../input/2b.in", 'r') as input:
+        with open("../output/2b.out", 'w') as output:
+            for line in input:
+                part_2b(line, output)
+
+
+if __name__ == "__main__":
+    # do_part_2a()
+    do_part_2b()
