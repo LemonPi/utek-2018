e9c74b6452935a49f14e1177d69f8241ce2d5df0
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-01-13 13:05:32 -0500

    part3: add ptb lambda from competition package

--- solution/part3.py
+++ solution/part3.py
@@ -3,6 +3,7 @@ import part2
 
 asciiForA = 0x41
 asciiForZ = 0x5a
+ptb_prob_weights = [1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,0.888889]
 
 def crack3a(ciphertext):
     bestScore = -1
@@ -10,7 +11,7 @@ def crack3a(ciphertext):
     bestKey = -1
     for i in range(0, 26):
         newText = part1.encryptBlock(ciphertext, [i], False)
-        newScore = part2.get_ptb_prob(newText)
+        newScore = part2.get_ptb_sentence_score(newText, ptb_prob_weights)
         if newScore > bestScore:
             bestScore = newScore
             bestText = newText
